3b90d73e902608993e243079cc24c8036f8dcb0f
Olav Gjerde <olav.gjerde@student.uib.no>
2018-03-06 16:19:31 +0100

    Constructed a maven module for the application; contains placeholders for now. Updated .gitignore

---
+++ application/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // placeholder
+    }
+
+    /**
+     * Example doc
+     * @param x
+     * @return double of x
+     */
+    public static int testThisMethod(int x){
+        return x*2;
+    }
+}
---
+++ application/src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Example for testing of a class, here: Main
+ */
+class MainTest {
+
+    @Test
+    void testThisMethodTest() {
+        assertEquals(4, Main.testThisMethod(2));
+    }
+}

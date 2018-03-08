596e9e650ce755e2f13e7da6c5432a10bdd8de81
xun007 <malin.jakobsen@uib.no>
2018-03-08 15:43:21 +0100

    Added position file and added some boolean variables

--- application/src/main/java/board/Pieces.java
+++ application/src/main/java/board/Pieces.java
@@ -1,4 +1,14 @@
 package board;
 
 public class Pieces {
+    boolean isWhite;
+    boolean hasMoved;
+    Position pos = new Position();
+
+
+    private static void getPosition(){
+    }
+
+
+
 }
---
+++ application/src/main/java/board/Position.java
@@ -0,0 +1,6 @@
+package board;
+
+public class Position {
+
+
+}

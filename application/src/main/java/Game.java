f8992fe5e942e9d8c20f1a837108f7249d001a7d
Olav <fruithut@tutanota.com>
2018-03-08 15:12:51 +0100

    Initial layout of Game-class.

---
+++ application/src/main/java/Game.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Game {
+    Board chessboard = new Board();
+
+    private boolean move(Move x){
+        if (checkMove(x)) {
+            // update piece position
+            return true;
+        }
+        
+        return false;
+    }
+
+    private boolean checkMove(Move move) {
+        return getLegalMoves(move.getPiece).contains(move.getNewPosition());
+    }
+
+    /**
+     * @return 1 if a winner is present, 0 if not
+     */
+    private int checkBoard(){
+        // check state after each 'update'
+        return 0;
+    }
+
+    private Board getChessboard() {
+        return chessboard;
+    }
+
+    private ArrayList<Position> getLegalMoves(Pieces piece) {
+        //check which type the piece is, return legal moves in relation to piece.
+    }
+}

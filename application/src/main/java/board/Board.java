56b79ebb27688cd36cee6bf340ef432e2ccf546b
Olav Gjerde <olav.gjerde@student.uib.no>
2018-03-13 14:40:52 +0100

    [Board.java] Added documentation to the Board-class.

--- application/src/main/java/board/Board.java
+++ application/src/main/java/board/Board.java
@@ -5,22 +5,31 @@ import pieces.*;
 import java.util.*;
 
 /**
- * TODO: DOCUMENT THIS CLASS
+ * This class represents a chessboard.
+ * A Map<Coordinate, Tile> is the base representation of the board layout,
+ * where a Coordinate object as key gives a Tile object as value with some content.
  */
 public class Board {
     private final Map<Coordinate, Tile> gameBoard;
     private final Collection<Piece> whitePieces;
     private final Collection<Piece> blackPieces;
+    final Collection<Move> whiteStandardLegalMoves;
+    final Collection<Move> blackStandardLegalMoves;
 
     public Board(Builder builder) {
         this.gameBoard = createGameBoard(builder);
         this.whitePieces = calculateActivePieces(this.gameBoard, Alliance.WHITE);
         this.blackPieces = calculateActivePieces(this.gameBoard, Alliance.BLACK);
 
-        final Collection<Move> whiteStandardLegalMoves = calculateLegalMoves(this.whitePieces);
-        final Collection<Move> blackStandardLegalMoves = calculateLegalMoves(this.blackPieces);
+        this.whiteStandardLegalMoves = calculateLegalMoves(this.whitePieces);
+        this.blackStandardLegalMoves = calculateLegalMoves(this.blackPieces);
     }
 
+    /**
+     * Creates a chessboard with a certain layout
+     * @param builder defines the layout/settings of the board
+     * @return a Map with coordinates as keys and tiles as values
+     */
     private Map<Coordinate, Tile> createGameBoard(Builder builder) {
         Map<Coordinate, Tile> coordTile = new HashMap<>();
         for (int i = 0; i < BoardUtils.getHeight(); i++) {
@@ -31,6 +40,12 @@ public class Board {
         return coordTile;
     }
 
+    /**
+     * Calculates how many active pieces there is of a given alliance on a board
+     * @param gameBoard board representation which holds pieces
+     * @param alliance to calculate for (black/white)
+     * @return a list of active pieces of a given alliance
+     */
     private static Collection<Piece> calculateActivePieces(Map<Coordinate, Tile> gameBoard, Alliance alliance) {
         final List<Piece> activePieces = new ArrayList<>();
         for (Tile tile : gameBoard.values()) {
@@ -44,6 +59,11 @@ public class Board {
         return Collections.unmodifiableList(activePieces);
     }
 
+    /**
+     * Generates a list of all legal moves given a collection of pieces
+     * @param pieces to evaluate legal moves for
+     * @return a list with all possible legal moves
+     */
     private Collection<Move> calculateLegalMoves(Collection<Piece> pieces) {
         final List<Move> legalMoves = new ArrayList<>();
         for (Piece piece : pieces) {
@@ -52,6 +72,36 @@ public class Board {
         return Collections.unmodifiableList(legalMoves);
     }
 
+    /**
+     * Returns a tile object given a coordinate
+     * @param tileCoordinate coordinate of tile to get
+     * @return the Tile-object at a given coordinate
+     */
+    public Tile getTile(final Coordinate tileCoordinate) {
+        return this.gameBoard.get(tileCoordinate);
+    }
+
+    /**
+     * Constructs a text visualisation of the board
+     * @return a string representation of the board-object
+     */
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < BoardUtils.getHeight(); i++) {
+            for (int j = 0; j < BoardUtils.getWidth(); j++) {
+                final String tileText = this.gameBoard.get(new Coordinate(j,i)).toString();
+                builder.append(String.format("%3s", tileText));
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Constructs a board with a standard chess layout
+     * @return a Board with the layout defined in the method below
+     */
     public static Board createStandardBoard() {
         final Builder builder = new Builder();
         // Black pieces
@@ -71,7 +121,6 @@ public class Board {
         builder.setPiece(new Pawn(new Coordinate(5,1), Alliance.BLACK));
         builder.setPiece(new Pawn(new Coordinate(6,1), Alliance.BLACK));
         builder.setPiece(new Pawn(new Coordinate(7,1), Alliance.BLACK));
-
         // White pieces
         builder.setPiece(new Rook(new Coordinate(0,7), Alliance.WHITE));
         builder.setPiece(new Knight(new Coordinate(1,7), Alliance.WHITE));
@@ -89,46 +138,48 @@ public class Board {
         builder.setPiece(new Pawn(new Coordinate(5,6), Alliance.WHITE));
         builder.setPiece(new Pawn(new Coordinate(6,6), Alliance.WHITE));
         builder.setPiece(new Pawn(new Coordinate(7,6), Alliance.WHITE));
-
         builder.setMoveMaker(Alliance.WHITE);
         return builder.build();
     }
 
-    public Tile getTile(final Coordinate tileCoordinate) {
-        return this.gameBoard.get(tileCoordinate);
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < BoardUtils.getHeight(); i++) {
-            for (int j = 0; j < BoardUtils.getWidth(); j++) {
-                final String tileText = this.gameBoard.get(new Coordinate(j,i)).toString();
-                builder.append(String.format("%3s", tileText));
-            }
-            builder.append("\n");
-        }
-        return builder.toString();
-    }
-
+    /**
+     * Helper class for constructing chessboards given a defined layout
+     */
     public static class Builder {
         Map<Coordinate, Piece> boardConfig;
         Alliance nextMoveMaker;
 
+        /**
+         * Construct a Builder object with an empty map.
+         */
         public Builder() {
             this.boardConfig = new HashMap<>();
         }
 
+        /**
+         * Adds a piece to the boardConfig-map
+         * @param piece to be placed
+         * @return Builder with this setting
+         */
         public Builder setPiece(Piece piece) {
             this.boardConfig.put(piece.getPieceCoordinate(), piece);
             return this;
         }
 
+        /**
+         * Set who makes the next move
+         * @param alliance who makes the next move
+         * @return Builder with this setting
+         */
         public Builder setMoveMaker(Alliance alliance) {
             this.nextMoveMaker = alliance;
             return this;
         }
 
+        /**
+         * Construct a new board object with the "settings" for this Builder object
+         * @return a Board object
+         */
         public Board build() {
             return new Board(this);
         }

f3d5e6d3ebe7b6547ec2fecdcaa3f81365d95e3c
Rune Vatne <rune.vatne@student.uib.no>
2018-03-17 12:13:37 +0100

    Refactoring of Score

--- application/src/main/java/player/Score.java
+++ application/src/main/java/player/Score.java
@@ -1,5 +1,10 @@
 package player;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Score {
     String username;
     int EloRating = findEloRating(username);
@@ -11,21 +16,24 @@ public class Score {
      * @param username
      * @return the elo-rating for the user as int, set the rating to 1500(placeholder) if name is not found.
      */
-    public int findEloRating(String username){
-        //TODO
-        String[2] userElo; //stores username/elo rating in [0]/[1]
+    //TODO: Handle exceptions, calculate new rating, fetch username
+    public int findEloRating(String username) {
+        String[] userElo = new String[2]; //stores username/elo rating in [0]/[1]
+        int elo = 1500; //Placeholder number
+
         try (BufferedReader br = new BufferedReader(new FileReader("username.txt"))) {
             String line = br.readLine();
 
-            while(line != null){
+            while (line != null) {
                 userElo = line.split("/");
-                int  = Integer.parseInt(userElo[1]);
-
+                int tempElo = Integer.parseInt(userElo[1]);
+                if (userElo[0].equals(username)) {
+                    elo = tempElo;
+                    break;
+                }
             }
+            return elo;
         }
-        ELO = 1500;
-        return ELO;
     }
-
 
 }

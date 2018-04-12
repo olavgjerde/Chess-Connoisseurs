155456cc13bf9f5c743678d1ae3c5384698c7143
Henrik Borgli <henrik.borgli@student.uib.no>
2018-04-12 14:21:04 +0200

    fresh clone of the current version, added soundclipmanager and resource files

---
+++ application/src/main/java/SoundClipManager.java
@@ -0,0 +1,33 @@
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+public class SoundClipManager {
+    private MediaPlayer mediaPlayer;
+
+    public SoundClipManager(String sound, boolean repeat, double volume, boolean enableSound) {
+        // If player has muted sounds in options do nothing
+        if (!enableSound) {
+            return;
+        }
+
+        // Load file from resource folder
+        String filePath = "sounds/" + sound;
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filePath).getFile());
+
+        Media hit = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(hit);
+        mediaPlayer.play();
+        mediaPlayer.setVolume(volume);
+        if (repeat) {
+            mediaPlayer.setCycleCount(mediaPlayer.INDEFINITE);
+        }
+        System.out.println("Playing: " + sound);
+    }
+
+    public void clear() {
+        mediaPlayer.dispose();
+    }
+}

875cd047be73061ad61d470a57fb014aaa3b4ee8
Olav Gjerde <olav.gjerde@student.uib.no>
2018-04-14 13:37:16 +0200

    [ChessMainRevamp.java] Fixed buttons clipping off screen

--- application/src/main/java/ChessMainRevamp.java
+++ application/src/main/java/ChessMainRevamp.java
@@ -701,7 +701,7 @@ public class ChessMainRevamp extends Application {
         //extra button styling
         HBox buttonContainer = new HBox(backButton, hintButton, forwardButton);
         buttonContainer.setAlignment(Pos.CENTER);
-        buttonContainer.setPadding(new Insets(380, 0, 0 , 0));
+        buttonContainer.setPadding(new Insets(200, 0, 0 , 0));
         buttonContainer.setSpacing(5);
         for (Node x : buttonContainer.getChildren()) {
             x.setStyle("-fx-focus-color: darkslategrey; -fx-faint-focus-color: transparent;");

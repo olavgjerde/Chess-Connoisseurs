abf64eb09e77ac2a0077191d228c3975c318a4ee
Olav Gjerde <olav.gjerde@student.uib.no>
2018-04-16 23:03:50 +0200

    [ChessMainRevamp.java] fixed a bug where the player could castle out of check

--- application/src/main/java/player/Player.java
+++ application/src/main/java/player/Player.java
@@ -38,8 +38,9 @@ public abstract class Player {
         // add castling moves to legal moves
         // allows the creation of boards without a king -> mostly for testing purposes
         if (king != null) {
-            legalMoves.addAll(calculateKingCastles(legalMoves, opponentMoves));
+            // calculate if player is in check before calculating castle moves
             this.isInCheck = !calculateAttacksOnCoordinate(this.playerKing.getPieceCoordinate(), opponentMoves).isEmpty();
+            legalMoves.addAll(calculateKingCastles(legalMoves, opponentMoves));
         }
         this.legalMoves = legalMoves;
     }

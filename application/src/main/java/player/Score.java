77f5e3696deaa719c6786edabeefcd8ce6b2b8ed
millianapia <millianapia@gmail.com>
2018-04-18 21:43:11 +0200

    Score should work with database
    should work

--- application/src/main/java/player/Score.java
+++ application/src/main/java/player/Score.java
@@ -1,7 +1,9 @@
 package player;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+
 import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -9,6 +11,8 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
+import com.mongodb.client.model.BulkWriteOptions;
+import com.mongodb.client.model.InsertOneModel;
 import org.bson.BSON;
 import org.bson.Document;
 import org.bson.types.ObjectId;
@@ -27,34 +31,34 @@ import static com.mongodb.client.model.Updates.set;
 public class Score {
     private HashMap<String, Integer> userRating = new HashMap<>();
     private HashMap<String, Stats> userStats = new HashMap<>();
-    private List<Document> highscoreDB = new ArrayList<Document>();
 
 
     /**
-     *  takes the old rating of both players, and the result of the game for both players,
-     1 for win, 0.5 for draw and 0 for loss
-     * @param username1 player1
-     * @param username2 player2
+     * takes the old rating of both players, and the result of the game for both players,
+     * 1 for win, 0.5 for draw and 0 for loss
+     *
+     * @param username1   player1
+     * @param username2   player2
      * @param gameResult1 gameresult for player 1
      * @param gameResult2 gameresult for player 2
      * @return Array containing updated ratings for both players
      */
-    public int [] matchRating(String username1, String username2, double gameResult1, double gameResult2){
+    public int[] matchRating(String username1, String username2, double gameResult1, double gameResult2) {
 
-        int score [] = new int[2];
+        int score[] = new int[2];
         double player1Rating = userRating.get(username1);
         double player2Rating = userRating.get(username2);
 
         int K = 32;
 
-        double R1 = Math.pow(10, (player1Rating/400));
-        double R2 = Math.pow(10, (player2Rating/400));
+        double R1 = Math.pow(10, (player1Rating / 400));
+        double R2 = Math.pow(10, (player2Rating / 400));
 
-        double E1 = R1/(R1+R2);
-        double E2 = R2/(R1+R2);
+        double E1 = R1 / (R1 + R2);
+        double E2 = R2 / (R1 + R2);
 
-        double newPlayer1Rating = player1Rating + K*(gameResult1 - E1);
-        double newPlayer2Rating = player2Rating + K*(gameResult2 - E2);
+        double newPlayer1Rating = player1Rating + K * (gameResult1 - E1);
+        double newPlayer2Rating = player2Rating + K * (gameResult2 - E2);
 
         score[0] = (int) newPlayer1Rating;
         score[1] = (int) newPlayer2Rating;
@@ -64,45 +68,54 @@ public class Score {
 
     /**
      * Updates a users highscore
+     *
      * @param username name of the user
      * @param newScore what to set the new score to
      */
-    public void updateHighscore(String username, int newScore){
+    public void updateHighscore(String username, int newScore) {
         userRating.put(username, newScore);
         writeHighscore();
     }
 
     /**
      * Returns a users rating
+     *
      * @param username the username of the user
      * @return the score of the given user
      */
-    public int getScore(String username){
+    public int getScore(String username) {
         return userRating.get(username);
     }
 
     /**
      * Returns a users game statistics
+     *
      * @param username the username of the user
      * @return a string with wins/draws/losses
      */
-    public String getStats (String username) {return userStats.get(username).getStats();}
+    public String getStats(String username) {
+        return userStats.get(username).getStats();
+    }
 
     /**
      * Returns a users game statistics
+     *
      * @param username the username of the user
      * @return a string with Wins: w, Draws: d, Losses: l
      */
-    public String getStatsVerbose (String username){return userStats.get(username).getStatsVerbose();}
+    public String getStatsVerbose(String username) {
+        return userStats.get(username).getStatsVerbose();
+    }
 
     /**
      * Adds a username with 1500 rating and enmpty stats to highscore.txt if the username does not already exist
+     *
      * @param username name of the user to add
      */
-    public void addUsername(String username){
-        if(!userRating.containsKey(username)){
+    public void addUsername(String username) {
+        if (!userRating.containsKey(username)) {
             userRating.put(username, 1500);
-            userStats.put(username, new Stats(0,0,0));
+            userStats.put(username, new Stats(0, 0, 0));
             writeHighscore();
         }
 
@@ -111,8 +124,8 @@ public class Score {
     /**
      * Reads highscore from MLab server. Iterates through all saved database entries
      */
-    public void readHighscore(){
-        MongoClientURI uri  = new MongoClientURI("mongodb://ccuser:ccpass@ds129706.mlab.com:29706/ccdb");
+    public void readHighscore() {
+        MongoClientURI uri = new MongoClientURI("mongodb://ccuser:ccpass@ds129706.mlab.com:29706/ccdb");
         MongoClient client = new MongoClient(uri);
         MongoDatabase db = client.getDatabase(uri.getDatabase());
         MongoCollection<Document> highscore = db.getCollection("highscore");
@@ -134,100 +147,70 @@ public class Score {
 
     }
 
-    /**
-     * //TODO: FIX AND CLEAN
-     */
-    private void writeHighscore(){
 
-        try {
-            PrintWriter out = new PrintWriter(new File("highscore.txt"));
-            for (String s : userRating.keySet()) {
-                String line = s + " " + userRating.get(s) + " " + userStats.get(s).getStats() + "\n";
-                out.write(line);
-            }
-            out.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("FileNotFound");
-        }
+    private void writeHighscore() {
 
+        List<Document> highscoreDB = new ArrayList<Document>();
 
-     /*   MongoClientURI uri  = new MongoClientURI("mongodb://ccuser:ccpass@ds129706.mlab.com:29706/ccdb");
+        MongoClientURI uri = new MongoClientURI("mongodb://ccuser:ccpass@ds129706.mlab.com:29706/ccdb");
         MongoClient client = new MongoClient(uri);
         MongoDatabase db = client.getDatabase(uri.getDatabase());
         MongoCollection<Document> highscore = db.getCollection("highscore");
-        MongoCursor<Document> cursor = highscore.find().iterator();
 
-            for (String s : userRating.keySet()) {
-                while (cursor.hasNext()) {
-                    Document doc = cursor.next();
+        highscore.drop();
 
-                   if (doc.containsValue(s)) {
-                        highscore.replaceOne(eq("_id", new ObjectId("5ad5d4131252d6204423dfa0")),
-                                        new Document("playername", s)
-                                        .append("score", userRating.get(s))
-                                        .append("stats", userStats.get(s).getStats()));
-                    } else {
-                        highscoreDB.add(new Document("playername", s)
-                                .append("score", userRating.get(s))
-                                .append("stats", userStats.get(s).getStats()));
-                        highscore.insertMany(highscoreDB);
-                    }
-                }
-            }
+        for (String s : userRating.keySet()) {
+            highscoreDB.add(new Document("playername", s)
+                    .append("score", userRating.get(s))
+                    .append("stats", userStats.get(s).getStats()));
 
-            client.close();*/
+
+        }
+        highscore.insertMany(highscoreDB);
+        client.close();
 
     }
-    //TODO: remove this before moving to master
-    //Template for inserting
-    /*        highscoreDB.add(new Document("playername", s)
-                                    .append("score", userRating.get(s))
-                                    .append("stats", userStats.get(s).getStats()));
-    *
-    * */
 
 
-
-
-    private Stats readStats(String stats){
+    private Stats readStats(String stats) {
         String[] temp = stats.split("/");
         int w = Integer.parseInt(temp[0]);
         int d = Integer.parseInt(temp[1]);
         int l = Integer.parseInt(temp[2]);
-        return new Stats(w,d,l);
+        return new Stats(w, d, l);
     }
 
-    public void addWin(String username){
+    public void addWin(String username) {
         userStats.get(username).addWin();
     }
 
-    public void addDraw(String username){
+    public void addDraw(String username) {
         userStats.get(username).addDraw();
     }
 
-    public void addLoss(String username){
+    public void addLoss(String username) {
         userStats.get(username).addLoss();
     }
 
-    public ArrayList<String> getScoreboard (){
+    public ArrayList<String> getScoreboard() {
         ArrayList<String> scoreboard = new ArrayList<>();
         HashMap<String, Integer> temp = new HashMap<>(userRating);
-        while(!temp.isEmpty()){
+        while (!temp.isEmpty()) {
             int hi = 0;
             String u = "";
-            for(String s : temp.keySet()){
-                if(temp.get(s) >= hi) {
+            for (String s : temp.keySet()) {
+                if (temp.get(s) >= hi) {
                     hi = temp.get(s);
                     u = s;
                 }
             }
             scoreboard.add(u);
-            temp.remove(u,hi);
+            temp.remove(u, hi);
         }
         return scoreboard;
     }
 
-    public int size(){
+    public int size() {
         return userRating.size();
     }
 
@@ -252,11 +235,11 @@ public class Score {
             losses++;
         }
 
-        private String getStats(){
-            return wins + "/"+ draws + "/" + losses;
+        private String getStats() {
+            return wins + "/" + draws + "/" + losses;
         }
 
-        private String getStatsVerbose(){
+        private String getStatsVerbose() {
             return "Wins: " + wins + ", Draws: " + draws + ", Losses: " + losses;
         }
     }
